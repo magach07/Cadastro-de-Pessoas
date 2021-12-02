60554b979cea72f46517ff6a9267813ce03b5e76
magach07 <jonathanmagacho@gmail.com>
2021-12-01 21:55:08 -0300

    Adicionar arquivos de projeto.

---
+++ CadastroPessoas/Controllers/PessoasController.cs
@@ -0,0 +1,83 @@
+﻿using CadastroPessoas.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace CadastroPessoas.Controllers {
+    public class PessoasController : Controller {
+
+        private readonly Contexto _context;
+
+        public PessoasController(Contexto context) {
+            _context = context;
+        }
+            
+         public async Task<IActionResult> Index() {
+            return View(await _context.Pessoas.ToListAsync());
+        }
+
+        [HttpGet]
+        public IActionResult CriarPessoa() {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CriarPessoa(Pessoa pessoa) {
+            if (ModelState.IsValid) {
+                _context.Add(pessoa);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+
+            } else {
+                return NotFound();
+            }
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> AtualizarPessoa(int? id) {
+            if (id != null) {
+                Pessoa pessoa = await _context.Pessoas.FindAsync(id);
+                return View(pessoa);
+            } else {
+                return NotFound();
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AtualizarPessoa(int? id, Pessoa pessoa) {
+            if(id != null) {
+                if (ModelState.IsValid) {
+                    _context.Update(pessoa);
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
+                } else {
+                    return NotFound();
+                }
+
+            } else {
+                return NotFound();
+            }
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> ExcluirPessoa(int? id) {
+            if(id != null) {
+                Pessoa pessoa = await _context.Pessoas.FindAsync(id);
+                return View(pessoa);
+            } else {
+                return NotFound();
+            }
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> ExcluirPessoa(int? id, Pessoa pessoa) {
+            if(id != null) {
+                _context.Remove(pessoa);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            } else {
+                return NotFound();
+            }
+        }
+    }
+}
---
+++ CadastroPessoas/Models/Contexto.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace CadastroPessoas.Models {
+    public class Contexto : DbContext {
+        public DbSet<Pessoa> Pessoas { get; set; }
+
+        public Contexto(DbContextOptions<Contexto> options) : base(options) {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+            IConfiguration configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", false, true)
+            .Build();
+
+            optionsBuilder.UseSqlServer(configuration.GetConnectionString("ServerConnection"));
+        }
+    }
+}
